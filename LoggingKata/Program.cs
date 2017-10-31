f39ae63d739ccce2103526b6ee79f268abf48392
Connor Davis <seafoamgreentele@gmail.com>
2017-10-31 11:00:48 -0700

    Refactored and made faster.

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -1,36 +1,185 @@
 ﻿using System;
+
 using System.Collections.Generic;
+
 using System.Linq;
+
 using System.Text;
+
 using System.Threading.Tasks;
+
 using log4net;
+
 using System.IO;
 
+using Geolocation;
+
+
+
 namespace LoggingKata
+
 {
+
     class Program
+
     {
+
         //Why do you think we use ILog?
+
+
+
         private static readonly ILog Logger =
+
             LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
+
+
         static void Main(string[] args)
+
         {
-            if (args.Length == 0)
+
+            var path = Environment.CurrentDirectory + "//Taco_Bell-US-AL-Alabama.csv";
+
+
+
+            if (path.Length == 0)
+
             {
+
                 Console.WriteLine("You must provide a filename as an argument");
-                Logger.Fatal("Cannot import without filename specified as an argument");
+
+                Logger.Fatal("Cannot import without filename specified in our path variable");
+
                 return;
+
             }
 
+
+
             Logger.Info("Log initialized");
-            var lines = File.ReadAllLines(args[0]);
+
+            Logger.Info("Grabbing from path" + path);
+
+
+
+            var lines = File.ReadAllLines(path);
+
+
+
+            if (lines.Length == 0)
+
+            {
+
+                Logger.Error("No locations to check for");
+
+            }
+
+            else if (lines.Length == 1)
+
+            {
+
+                Logger.Warn("Only one location provided");
+
+            }
+
             var parser = new TacoParser();
-            var locations = lines.Select(line => parser.Parse(line));
+
+            Logger.Debug("Initialized our Parser");
+
+
+
+            var locations = lines.Select(line => parser.Parse(line))
+
+                .OrderBy(loc => loc.Location.Latitude)
+
+                .ThenBy(loc => loc.Location.Longitude)
+
+                .ToArray();
+
+
 
             //TODO:  Find the two TacoBells in Alabama that are the furthurest from one another.
-            //HINT:  You'll need two nested forloops
+
+            ITrackable a = null;
+
+            ITrackable b = null;
+
+            double distance = 0;
+
+            foreach (var locA in locations)
+
+            {
+
+                var origin = new Coordinate
+
+                {
+
+                    Latitude = locA.Location.Latitude,
+
+                    Longitude = locA.Location.Longitude
+
+                };
+
+                foreach (var locB in locations)
+
+                {
+
+                    var dest = new Coordinate
+
+                    {
+
+                        Latitude = locB.Location.Latitude,
+
+                        Longitude = locB.Location.Longitude
+
+                    };
+
+
+
+                    var nDist = GeoCalculator.GetDistance(origin, dest);
+
+
+
+                    if (nDist > distance)
+
+                    {
+
+                        a = locA;
+
+                        b = locB;
+
+                        distance = nDist;
+
+                    }
+
+                }
+
+            }
+
+
+
+            if (a == null || b == null)
+
+            {
+
+                Logger.Error("Failed to find furthest locations");
+
+                Console.WriteLine("Couldn't find the distance between furthest locations");
+
+                Console.ReadLine();
+
+            }
+
+
+
+            Console.WriteLine($"the two taco bell's that are farthest apart are {a.Name} and : {b.Name}");
+
+            Console.WriteLine($"These two locations are {distance} miles away");
+
+            Console.ReadLine();
 
         }
+
     }
+
 }
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -1,26 +1,113 @@
-﻿using System.Collections;
+﻿using System;
+
+using System.Collections;
+
 using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Linq.Expressions;
+
+using System.Runtime.Remoting.Messaging;
+
 using log4net;
 
-namespace LoggingKata
-{
-    /// <summary>
-    /// Parses a POI file to locate all the TacoBells
-    /// </summary>
-    public class TacoParser
-    {
-        public TacoParser()
-        {
 
-        }
+
+namespace LoggingKata
+
+{
+
+    /// <summary>
+
+    /// Parses a POI file to locate all the TacoBells
+
+    /// </summary>
+
+    public class TacoParser
+
+    {
 
         private static readonly ILog Logger =
+
             LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
+
+
         public ITrackable Parse(string line)
+
         {
-            //DO not fail if one record parsing fails, return null
-            return null; //TODO Implement
+
+            var listOfTacoBell = line.Split(',');
+
+            if (listOfTacoBell.Length < 3)
+
+            {
+
+                Logger.Error("Must have at least three items in the array");
+
+                return null;
+
+            }
+
+            double lon = 0;
+
+            double lat = 0;
+
+
+
+            try
+
+            {
+
+                Logger.Debug("Attempt parsing longitude");
+
+                lon = double.Parse(listOfTacoBell[0]);
+
+                Logger.Debug("Attempt parsing latitude");
+
+                lat = double.Parse(listOfTacoBell[1]);
+
+            }
+
+            catch (Exception e)
+
+            {
+
+                Logger.Error("Failed to parse the location", e);
+
+                Console.WriteLine(e);
+
+                throw;
+
+            }
+
+
+
+            var tB = new TacoBell
+
+            {
+
+                Name = listOfTacoBell[2].Split('.')[0].Replace("/", "").Replace("\"", ""),
+
+                Location = new Point
+
+                {
+
+                    Longitude = lon,
+
+                    Latitude = lat
+
+                }
+
+            };
+
+
+
+            return tB;
+
         }
+
     }
+
 }
